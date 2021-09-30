f72199f888eeb5dd97c3811cb725866931e4c3c3
Alexey Zabashta <gwynblayde@mail.ru>
2021-09-30 18:47:13 +0300

    docs:add solution to transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,64 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let out=[];
+  if (!Array.isArray(arr))
+    throw"'arr' parameter must be an instance of the Array!";
+  else{
+    
+    for (let i=0;i<arr.length;i++){
+      if ((arr[i]==='--discard-next')&&(arr[i+2]==='--double-prev')){
+        out.push(arr[i+3]);
+        i+=3;
+        continue;
+      }
+      if ((arr[i]==='--double-next')&&(arr[i+2]==='--double-prev')){
+        out.push(arr[i+1]);
+        out.push(arr[i+1]);
+        out.push(arr[i+1]);
+        i+=3;
+        continue;
+      }
+      if ((arr[i]==='--discard-next')&&(arr[i+2]==='--discard-prev')){
+        out.push(arr[i+3]);
+        i+=3;
+        continue;
+      }
+      if ((arr[i]==='--double-next')&&(arr[i+2]==='--discard-prev')){
+        out.push(arr[i+1]);
+        i+=2;
+        continue;
+      }
+
+      if (arr[i]==='--discard-next')
+        {i++;
+          continue;}
+      if (arr[i]==='--discard-prev')
+        {out.pop();
+          continue;}
+      if (arr[i]==='--double-next'){
+        out.push(arr[i+1]);
+        out.push(arr[i+1]);
+        i++;
+        continue;
+      }
+      if (arr[i]==='--double-prev'){
+        out.push(arr[i-1]);
+        out.push(arr[i-1]);
+        continue;
+      }
+      if ((isNaN(Number(arr[i]))===true)||(arr[i]===''))
+       {continue;}
+      else
+      {out.push(arr[i]);}
+    }
+
+  }
+  return out;
 }
+
+
+
+
+
