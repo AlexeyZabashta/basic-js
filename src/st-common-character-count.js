807a327e8f6fadbac6e0fdd3778165945bee2f37
Alexey Zabashta <gwynblayde@mail.ru>
2021-10-01 23:20:03 +0300

    docs: add solution to getCommonCharacterCount, deleteDigit

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+	let mass1=s1.split('');
+	let mass2=s2.split('');
+	let count1={};
+	let count2={};
+	for (let i=0;i<mass1.length;i++){
+		let m=mass1[i];
+		if (!(m in count1))
+			count1[m]=0;
+		count1[m]++;
+	}
+	for (let i=0;i<mass2.length;i++){
+		let m=mass2[i];
+		if (!(m in count2))
+			count2[m]=0;
+		count2[m]++;
+	}
+	//console.log(count1,count2);
+	let keys1=Object.keys(count1).sort();
+	let keys2=Object.keys(count2).sort();
+	//console.log(keys1,keys2);
+	let out=0;
+	for (let k=0;k<keys1.length;k++){
+		if (keys1[k] in count2)
+			out+=Math.min(count1[keys1[k]],count2[keys1[k]]);
+		else 
+			continue;
+	}
+	return out;
+
+ 	
 }
+
--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  let str=n+'';
+  let mass=[];
+  for (let i=0;i<str.length;i++){
+    if (i===0)
+    {mass.push(str.substr(1));
+      
+      continue;}
+    if (str.length-i===1)
+    {mass.push(str.substr(0,i));
+      
+      continue;}
+    mass.push(str.substr(0,i)+str.substr(i+1));
+    }
+    function max(r,v,i,a){
+      return Math.max(Number(r),Number(v));
+    }
+  
+  return mass.reduce(max);
 }
+
